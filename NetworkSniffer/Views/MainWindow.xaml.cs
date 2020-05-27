6099d24c4c9dad7a19e3bc9647e09d9d2a4df87b
Felipe Fagundes Scalco <scalfe@2020spaces.com>
2020-05-26 22:41:53 -0300

    FRIST !1

---
+++ NetworkSniffer/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace NetworkSniffer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
